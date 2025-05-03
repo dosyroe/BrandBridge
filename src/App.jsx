93c71d33b118c366edd449f4dcc8c3595f978f1a
Dosyroe <dosyroe@gmail.com>
2025-05-03 22:31:06 +0300

    Reworking and fixing page animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import React, { useRef } from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
 import MainPage from "./pages/MainPage";
@@ -9,40 +8,18 @@ import NotFound from "./pages/404";
 import ContactsPage from "./pages/ContactsPage";
 import DeliveryPage from "./pages/DeliveryPage";
 
-function AnimatedRoutes() {
-  const location = useLocation();
-  const nodeRef = useRef(null);
-
-  return (
-    <TransitionGroup>
-      <CSSTransition
-        key={location.key}
-        classNames="fade"
-        timeout={300}
-        nodeRef={nodeRef}
-      >
-        <div ref={nodeRef}>
-          <Routes location={location}>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/support" element={<SupportPage />} />
-            <Route path="/contacts" element={<ContactsPage />} />
-            <Route path="/delivery" element={<DeliveryPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </CSSTransition>
-    </TransitionGroup>
-  );
-}
-
 function App() {
   return (
     <Router>
-      <AnimatedRoutes />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/support" element={<SupportPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+        <Route path="/delivery" element={<DeliveryPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </Router>
-
-    
   );
-}
+};
 
 export default App;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,23 +1,28 @@
+import React, { useState, useEffect } from "react";
+import "../App.css";
 
-import '../App.css'
-
-
-import Header from '../components/mainPage/Header/Header';
-import Footer from '../components/mainPage/Footer/Footer'
-import MainBlock from '../components/mainPage/MainBlock/MainBlock'
-
-
+import Header from "../components/mainPage/Header/Header";
+import Footer from "../components/mainPage/Footer/Footer";
+import MainBlock from "../components/mainPage/MainBlock/MainBlock";
 
 function MainPage() {
+  const [isVisible, setIsVisible] = useState(false);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 200);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <>
+    <div className={`page-content ${isVisible ? "visible" : ""}`}>
       <Header />
       <MainBlock />
       <Footer />
-    </>
-  )
+    </div>
+  );
 }
 
 export default MainPage;
